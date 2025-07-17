32db98705629fff82139a751841a33ddb424bf46
cherish0-0 <jiyu01160@naver.com>
2025-07-18 00:04:50 +0900

    initial commit

---
+++ src/main/java/com/betest2/be_test2/BeTest2Application.java
@@ -0,0 +1,13 @@
+package com.betest2.be_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/betest2/be_test2/BeTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betest2.be_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
